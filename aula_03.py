7bf76ab94ed01e6318ee5e387e751704e805c9ae
m4teuss <mateuus.s.silva@gmail.com>
2022-04-26 09:14:59 -0300

    Add ex3

--- aula_03.py
+++ aula_03.py
@@ -1,5 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from time import sleep
 
 browser = webdriver.Chrome(executable_path=r'./chromedriver/chromedriver.exe')
-browser.get('https://google.com')
+url = 'https://curso-python-selenium.netlify.app/aula_03.html'
+browser.get(url)
+
+sleep(1)
+
+
+
+a = browser.find_element_by_tag_name('a')
+
+
+for click in range(10):
+    ps = browser.find_elements_by_tag_name('p')
+    a.click()
+    # [-1] -> pegando o ultimo p clicado 
+    print(f'valor do ps {ps[-1].text} valor do click {click}')
+    print(f'Os valores são iguais {ps[-1].text == str(click)}')
+
+
+
+
